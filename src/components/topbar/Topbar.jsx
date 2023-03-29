ad5f27b5beae2d5eeef6224ffaae103c51832e70
Ghulam Rabbani <rabbani4007@gmail.com>
2023-03-29 17:14:24 +0530

    added the top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import "./topbar.css";
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">ReactAdmin</span>
+          <span className="logo">reactadmin</span>
         </div>
         <div className="topRight">
-          <div className="topbarIcons">
-<NotificationsNoneIcon/>
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon />
+            <span className="topIconBadge">2</span>
           </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+          </div>
+          <img
+            src="https://avatars.githubusercontent.com/u/60386326?v=4"
+            alt=""
+            className="topbarAvatar"
+          />
         </div>
       </div>
     </div>
