7b3265ea828adbcc235aac9c68055047ddf01dfb
Ghulam Rabbani <rabbani4007@gmail.com>
2023-03-30 15:33:33 +0530

    updated the chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -4,6 +4,8 @@ import {
   LineChart,
   Line,
   XAxis,
+  YAxis,
+  Legend,
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
@@ -11,43 +13,43 @@ import {
 
 const data = [
   {
-    name: "Page A",
+    name: "Jan",
     "Active Users": 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
+    name: "Feb",
     "Active Users": 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
+    name: "Mar",
     "Active Users": 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
+    name: "Apr",
     "Active Users": 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
+    name: "May",
     "Active Users": 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: "Page F",
+    name: "Jun",
     "Active Users": 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: "Page G",
+    name: "Jul",
     "Active Users": 3490,
     pv: 4300,
     amt: 2100,
@@ -60,6 +62,9 @@ export default function Chart() {
       <ResponsiveContainer width="100%" aspect={4 / 1}>
         <LineChart data={data}>
           <XAxis dataKey="name" stroke="#5550bd" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
           <Line type="monotone" dataKey="Active Users" stroke="#5550bd" />
         </LineChart>
       </ResponsiveContainer>
